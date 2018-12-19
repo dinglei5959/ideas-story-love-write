c624dc9b21f64d4f1bc47c90a86344196a90e31d
dinglei <dingl@dtdream.com>
2018-12-19 17:42:55 +0800

    提交docs

---
+++ docs/precache-manifest.c120551a44ae1f2d4502c13b7830b9b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bc0e6e4d3455e39328d60083f4b72e",
+    "url": "/static/media/sangu.79bc0e6e.jpg"
+  },
+  {
+    "revision": "9c018cb2ad6f73e9c9a9",
+    "url": "/static/css/main.b8e8aed7.chunk.css"
+  },
+  {
+    "revision": "044ff4002cacc4bdf86a",
+    "url": "/static/js/1.044ff400.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d18f28be7a8c1f14548545a447e493f0",
+    "url": "/static/media/home.d18f28be.jpg"
+  },
+  {
+    "revision": "5db457a20d823c05498e268743d6e389",
+    "url": "/static/media/losanji.5db457a2.jpg"
+  },
+  {
+    "revision": "9c018cb2ad6f73e9c9a9",
+    "url": "/static/js/main.9c018cb2.chunk.js"
+  },
+  {
+    "revision": "b9607351a2480724ba9de57751eb5685",
+    "url": "/static/media/archive.b9607351.svg"
+  },
+  {
+    "revision": "24537465e92b0c41a1b9b2ea800b882c",
+    "url": "/static/media/author.24537465.svg"
+  },
+  {
+    "revision": "32a8a2d7f2594cf42f10b33fcb03e634",
+    "url": "/static/media/home.32a8a2d7.svg"
+  },
+  {
+    "revision": "a5dc9293a163eb08438754e3da0e74c7",
+    "url": "/static/media/subscribe.a5dc9293.svg"
+  },
+  {
+    "revision": "a2551ddebc9f6d81351bcefc5670703a",
+    "url": "/static/media/tag.a2551dde.svg"
+  },
+  {
+    "revision": "8d0b7678204f2929d4ae8df52e1ac0ed",
+    "url": "/index.html"
+  }
+];
